ccefbfdad169e9a17e4d5e56bba5b04f4194441b
sarasate <luis@superlumen.io>
2018-02-28 18:00:59 +0100

    Add post title

--- gatsby/src/templates/post.js
+++ gatsby/src/templates/post.js
@@ -1,23 +1,17 @@
-import React from 'react';
-import Helmet from 'react-helmet';
-import Link from 'gatsby-link';
-
+import React from "react";
+import Helmet from "react-helmet";
+import Link from "gatsby-link";
 
 export default function Template({ data, pathContext }) {
   const { markdownRemark } = data;
-  const {
-    frontmatter,
-    html,
-  } = markdownRemark;
+  const { frontmatter, html } = markdownRemark;
 
   return (
     <div>
-      <p
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
+      <h1>{frontmatter.title}</h1>
+      <p dangerouslySetInnerHTML={{ __html: html }} />
       <Link to="/">Home</Link>
     </div>
-
   );
 }
 
