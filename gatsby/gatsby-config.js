0dadcc7ab3b8782fdeaf3253fcf2540f3ad7874c
sarasate <luis@superlumen.io>
2018-02-28 18:23:18 +0100

    Add gatsby plugin for styled-components

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-resolve-src",
+    "gatsby-plugin-styled-components",
     "gatsby-plugin-react-next",
     "gatsby-plugin-catch-links",
     {
