39061d6d38ddbfe0d7a7fe1a88cb25913775abfb
Callum Macdonald <git@callum-macdonald.com>
2018-03-14 14:08:30 +0100

    Enable syntax highlighting.

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -40,6 +40,12 @@ module.exports = {
             options: {
               linkImagesToOriginal: false
             }
+          },
+          {
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              classPrefix: "language-"
+            }
           }
         ]
       }
