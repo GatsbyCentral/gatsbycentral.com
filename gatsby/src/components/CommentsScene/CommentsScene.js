3adfe4665f34ac88682907b52cd208144e429d17
sarasate <luis@superlumen.io>
2018-05-17 21:41:25 +0200

    Fix react key for comments

--- gatsby/src/components/CommentsScene/CommentsScene.js
+++ gatsby/src/components/CommentsScene/CommentsScene.js
@@ -5,7 +5,7 @@ import CommentForm from "./components/CommentForm";
 import Comment from "./components/Comment";
 
 const renderComments = map(comment => (
-  <Comment key={comment.node.email} comment={comment} />
+  <Comment key={comment.node.date} comment={comment} />
 ));
 
 const CommentsScene = ({ postId, comments }) => {
