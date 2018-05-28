2e8c1985b4f61ee2f838612e355bd20cbc3e672a
sarasate <luis@superlumen.io>
2018-05-28 16:15:36 +0200

    Add snippet page

---
+++ gatsby/src/pages/snippets.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Meta from '../components/Meta/Meta'
+
+const Snippets = () => {
+  return <div>
+    <Meta data={{title:"Snippets"}}/>
+    <h1>Snippets</h1>
+  </div>
+};
+
+export default Snippets;
