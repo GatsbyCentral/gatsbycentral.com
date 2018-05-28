e6557fb3c5dbe81eea3c52f76c4f983c47733d0f
sarasate <luis@superlumen.io>
2018-05-28 18:43:13 +0200

    Filter blog post pages

--- gatsby/src/pages/posts.js
+++ gatsby/src/pages/posts.js
@@ -1,17 +1,16 @@
 import React from "react";
-import Meta from 'components/Meta/Meta'
+import Meta from "components/Meta/Meta";
 
 import H1 from "components/Style/Headings/H1.js";
 import PostList from "components/Post/PostList/PostList.js";
 
 import Container from "components/Layout/Container";
 
-
 const Posts = props => {
   const { edges: posts } = props.data.allMarkdownRemark;
   return (
     <Container>
-        <Meta data={{title:'All Posts'}}/>
+      <Meta data={{ title: "All Posts" }} />
       <H1 centered={true}>All Posts</H1>
       <PostList posts={posts} />
     </Container>
@@ -20,7 +19,10 @@ const Posts = props => {
 
 export const query = graphql`
   query allPosts {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      filter: { fields: { collection: { eq: "pages" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
       edges {
         node {
           id
