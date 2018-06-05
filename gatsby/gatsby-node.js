2f83120ecfb3a9256bf8521e0f1b2e997959ae59
Callum Macdonald <git@callum-macdonald.com>
2018-06-05 19:28:36 +0200

    Prettier.

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,42 +1,43 @@
-const path = require('path');
+const path = require("path");
 
 exports.createPages = ({ boundActionCreators, graphql }) => {
   const { createPage } = boundActionCreators;
 
   const blogPostTemplate = path.resolve(`src/templates/post.js`);
-  return graphql(`{
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] }
-      limit: 1000
-    ) {
-      edges {
-        node {
-          html
-          id
-          frontmatter {
-            date
-            path
-            title
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            html
+            id
+            frontmatter {
+              date
+              path
+              title
+            }
           }
         }
       }
     }
-  }`)
-    .then(result => {
-      if (result.errors) {
-        return Promise.reject(result.errors)
-      }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
 
-      const posts = result.data.allMarkdownRemark.edges;
+    const posts = result.data.allMarkdownRemark.edges;
 
-      // Create pages for each markdown file.
-      posts.forEach(({ node }, index) => {
-        createPage({
-          path: node.frontmatter.path,
-          component: blogPostTemplate,
-        });
+    // Create pages for each markdown file.
+    posts.forEach(({ node }, index) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: blogPostTemplate
       });
+    });
 
-      return posts;
-    })
+    return posts;
+  });
 };
