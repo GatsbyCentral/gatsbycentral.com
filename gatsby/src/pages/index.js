ffd37f21196e817ea9b5a3fe79172344ede5ee79
Callum Macdonald <git@callum-macdonald.com>
2018-06-15 20:06:30 +0200

    Drop redundant Header components.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import Meta from "components/Meta/Meta";
 
 import FeaturedPosts from "components/FeaturedPosts";
-import Header from "components/Header";
 import Container from "components/Layout/Container";
 
 // SEO meta data for the homepage.
@@ -12,12 +11,44 @@ const meta = {
   description: "The Community for Gatsby Developers"
 };
 
-export default function Index(props) {
+const Posts = props => {
+  const { edges: posts } = props.data.posts;
+  return (
+    <Container>
+      <Meta data={{ title: "All Posts" }} />
+      <PostList posts={posts} />
+    </Container>
+  );
+};
+
+const Index = props => {
   return (
     <Container>
       <Meta data={meta} />
-      <Header />
+      <h1>Become a master of GatsbyJS</h1>
+      <p>Build static sites with the stack you love</p>
       <FeaturedPosts />
     </Container>
   );
-}
+};
+
+export default Index;
+
+export const query = graphql`
+  query indexPosts {
+    posts: allMarkdownRemark(
+      filter: { fields: { collection: { eq: "pages" } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`;
